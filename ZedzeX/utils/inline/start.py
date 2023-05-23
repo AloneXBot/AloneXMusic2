80ddc16277dbdbfe7e69e440711d0f3e51d23d6d
AloneXBot <132041403+AloneXBot@users.noreply.github.com>
2023-05-23 15:42:04 +0530

    Update start.py

--- ZedzeX/utils/inline/start.py
+++ ZedzeX/utils/inline/start.py
@@ -50,7 +50,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="✨ ᴏᴡɴᴇʀ ✨", user_id=OWNER),
+                text="✨ ᴏᴡɴᴇʀ ✨", url=f"{config.OWNER_ID},
             ),
             InlineKeyboardButton(
                 text="❄️ sᴏᴜʀᴄᴇ ❄️", url=f"https://github.com/AloneXBot/AloneXMusic",
