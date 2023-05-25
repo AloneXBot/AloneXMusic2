cb913cbaa1ada092fd0c8eb6f9040b4bc8543d8c
AloneXBot <132041403+AloneXBot@users.noreply.github.com>
2023-05-25 15:42:44 +0530

    Update start.py

--- ZedzeX/plugins/start.py
+++ ZedzeX/plugins/start.py
@@ -206,7 +206,8 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                return await message.reply_photo(
+                await message.reply_sticker("CAACAgUAAxkBAAITqWRvMjpYtneeElizLBp5-DLfZq0fAAJ1CAACTmGBV5_wreIbydDZLwQ")
+                await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
                         config.MUSIC_BOT_NAME
