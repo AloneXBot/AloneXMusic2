2bf03939e547dcb6ac32b7e83fd55dd95e886b76
AloneXBot <132041403+AloneXBot@users.noreply.github.com>
2023-05-25 15:33:10 +0530

    Update start.py

--- ZedzeX/plugins/start.py
+++ ZedzeX/plugins/start.py
@@ -43,6 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
+            await message.reply_sticker("CAACAgUAAxkBAAITqWRvMjpYtneeElizLBp5-DLfZq0fAAJ1CAACTmGBV5_wreIbydDZLwQ")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
